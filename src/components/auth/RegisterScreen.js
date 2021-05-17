628066816376a0eddc2302dd32b2082da48dbd63
Saúl Laínez <saullainez@hotmail.es>
2021-05-17 15:38:46 -0600

    Estilos en el register screen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>Register Screen</h1>
-        </div>
+        <>
+            <h3 className="auth__title mb-5">Registrarse</h3>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Nombre"
+                    name="name"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+                <input
+                    type="text"
+                    placeholder="Correo electrónico"
+                    name="email"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    name="password"
+                    className="auth__input"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirmar contraseña"
+                    name="password2"
+                    className="auth__input"
+                />
+                <button
+                    className="btn btn-primary btn-block mb-5"
+                    type="submit"
+
+                >
+                    Registrarse
+                </button>
+
+                <div className="text-center">
+                    <Link 
+                        to="/auth/login"
+                        className="link"
+                    >
+                        Iniciar sesión
+                    </Link>
+                </div>
+
+            </form>
+        </>
     )
 }
