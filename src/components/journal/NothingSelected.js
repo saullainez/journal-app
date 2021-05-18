20983decfc6e4b9756fd8a0d2f44fa8d388af1fb
Saúl Laínez <saullainez@hotmail.es>
2021-05-18 15:12:05 -0600

    Componente cuando no hay nada seleccionado

---
+++ src/components/journal/NothingSelected.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NothingSelected = () => {
+    return (
+        <div className="nothing__main-content">
+            <p>
+                Selecciona algo 
+                <br />
+                o crea una nueva entrada
+            </p>
+
+            <i className="far fa-star fa-4x mt-5"></i>
+        </div>
+    )
+}
