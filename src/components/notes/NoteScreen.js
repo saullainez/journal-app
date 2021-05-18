c06ba424d02b44f87795d7fc7853665005e79a4d
Saúl Laínez <saullainez@hotmail.es>
2021-05-18 15:41:16 -0600

    Estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="Qué pasó hoy"
+                    className="notes__text-area"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://e00-ar-marca.uecdn.es/claro/assets/multimedia/imagenes/2020/06/21/15926966530661.jpg"
+                        alt="Imagen"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
