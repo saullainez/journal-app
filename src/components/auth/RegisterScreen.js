0c1eaac756af4403c5ff258b04ad4b5d837cf9bf
Saúl Laínez <saullainez@hotmail.es>
2021-05-20 15:27:17 -0600

    useSelector
    Obtener información del state

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
 import validator from 'validator'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { removeError, setError } from '../../actions/ui';
 
 export const RegisterScreen = () => {
 
     const dispatch = useDispatch();
+    const { msgError } = useSelector( state => state.ui );
 
     const [ formValues, handleInoutChange ] = useForm({
         name: 'Saul',
@@ -47,9 +48,14 @@ export const RegisterScreen = () => {
             <h3 className="auth__title mb-5">Registrarse</h3>
             <form onSubmit={ handleRegister }> 
 
-                <div className="auth__alert-error">
-                    Error
-                </div>
+                {
+                    msgError &&
+                    (
+                        <div className="auth__alert-error">
+                            { msgError }
+                        </div>
+                    )
+                }
 
                 <input
                     type="text"
