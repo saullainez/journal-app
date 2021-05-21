6086d59aab899ffedb56b661c3c7b00602aeeec1
Saúl Laínez <saullainez@hotmail.es>
2021-05-21 10:16:40 -0600

    Logout del firebase

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
+
+    const dispatch = useDispatch();
+    
+    const handleLogout = () => {
+        dispatch(startLogout());
+    }
+
     return (
         <aside className="journal__sidebar">
             <div className="journal__sidebar-navbar">
@@ -9,7 +18,9 @@ export const Sidebar = () => {
                     <i className="far fa-moon"></i>
                     <span> Saul</span>
                 </h3>
-                <button className="btn">
+                <button className="btn"
+                    onClick={handleLogout}
+                >
                     Logout
                 </button>
             </div>
