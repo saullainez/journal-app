6c64c15a8c219b6f5cce10b35ce7c9b7812fcf98
Saúl Laínez <saullainez@hotmail.es>
2021-05-21 10:54:15 -0600

    Mensaje de error

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,7 +2,10 @@ import {types} from '../types/types';
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { finishLoading, startLoading } from './ui';
 
+import Swal from 'sweetalert2';
+
 export const startLoginEmailPassword = (email, password) => {
+    let msjErr = "";
     return (dispatch) => {
         dispatch(startLoading());
         firebase.auth().signInWithEmailAndPassword( email, password )
@@ -11,13 +14,16 @@ export const startLoginEmailPassword = (email, password) => {
                 dispatch(finishLoading());
             })
             .catch(e => {
+                msjErr = (e.code === "auth/user-not-found" || e.code === "auth/wrong-password") ? "Usuario o contraseña incorrectos" : "Ocurrió un error";
                 console.log(e);
                 dispatch(finishLoading());
+                Swal.fire('Error', msjErr, 'error');
             });
     }
 }
 
 export const startRegisterWithEmailPasswordName = (email, password, name) => {
+    let msjErr = "";
     return ( dispatch ) => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then( async ({user}) => {
@@ -27,7 +33,9 @@ export const startRegisterWithEmailPasswordName = (email, password, name) => {
                 );
             })
             .catch(e => {
+                msjErr = e.code === "auth/email-already-in-use"  ? "El correo ya está en uso por otra cuenta" : "Ocurrió un error";
                 console.log(e);
+                Swal.fire('Error', msjErr, 'error');
             });
     }
 }
