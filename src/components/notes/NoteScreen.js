8ede8dd7cf1048a2a4710c8ce03f6d73ab2dca07
Saúl Laínez <saullainez@hotmail.es>
2021-05-22 10:50:37 -0600

    Cambiar la nota activa al cambiar la sección

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
@@ -6,9 +6,19 @@ import { NotesAppBar } from './NotesAppBar'
 export const NoteScreen = () => {
 
     const { active:note } = useSelector( state => state.notes );
-    const [ formValues, handleInputChange ] = useForm(note);
-
+    const [ formValues, handleInputChange, reset ] = useForm(note);
     const { body, title } = formValues;
+
+    const activeId = useRef(note.id);
+
+    useEffect(() => {
+
+        if(note.id !== activeId.current){
+            reset( note );
+            activeId.current = note.id;
+        }
+
+    }, [note, reset])
 
     return (
         <div className="notes__main-content">
